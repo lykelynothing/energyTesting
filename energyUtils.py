ebdd77dae687999f13f31f82837d8e1a2fd45238
Luca Mautino <luca.mautino@gmail.com>
2024-12-13 12:09:19 +0100

    Better file naming, watch out for backlashes

--- energyUtils.py
+++ energyUtils.py
@@ -37,7 +37,7 @@ def test_pgd_impact(steps : List[int],
     a list where the mean energies for each kind of PGD step will be stored.
     Now also stores difference between mean normal energy and mean adversarial energy.
     '''
-
+    
     for i in range(len(steps)):
 
       start_time = time.time()
@@ -53,10 +53,10 @@ def test_pgd_impact(steps : List[int],
       attack = PGD(model, steps=steps[i])
       attack.set_normalization_used((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 
-      for batch, (X, y) in enumerate(dataloader):  
+      for batch, (X, y) in enumerate(dataloader):
         
         time_prev = time.time()
-        
+
         X = X.to(device)
         y = y.to(device)
         adv = attack(X, y)
@@ -80,12 +80,13 @@ def test_pgd_impact(steps : List[int],
         time_est = 0.9 * time_curr + 0.1 * time_est_prev
         time_est_prev = time_est
 
-        bar = '■' * int(((batch + 1) / (len(dataloader))) * 20)
+        bar = '#' * int(((batch + 1) / (len(dataloader))) * 20)
         print(f"\r| Current steps: {steps[i]} [{bar}] {( (batch + 1) / len(dataloader) ) * 100:.1f}% | Est. Time{(time_est * len(dataloader))/ 60: .2f} min | Elapsed {(time_prev - start_time) / 60 : .2f} min | Adv Acc: {(acc_adv / ((batch+1) * dataloader.batch_size)) * 100:.2f}% | Correct : {acc_adv} | Delta : {delta / ( (batch + 1) * dataloader.batch_size) :.2f}"
         , flush=True, end='')
 
       # change filepath accordingly
-      with open(f"/Users/luca/Documents/Thesis/energyTesting/means/Resnets/{model_name}_{model_rob}_Resnet56-10.txt", "a") as file:
+      path = os.path.join(os.getcwd(), 'means\\Resnets')
+      with open(f"{path}\\{model_name}_{model_rob}.txt", "a") as file:
         file.write(f"\nSteps {steps[i]} mean_en : ")
         file.write(str(mean_en / (len(dataloader) * dataloader.batch_size)) + '\n')
         file.write(f"Adversarial accuracy: {100 * acc_adv / (len(dataloader) * dataloader.batch_size) :.2f}%\n")
